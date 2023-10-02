b8357fa23f33781897a04cdea679061ac8e893c1
zilladeepak <zilladeepak@gmail.com>
2023-10-02 21:51:20 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const button = document.querySelectorAll('button');
+const display = document.querySelector('.screen');
+
+const lastOperator = false
+
+function calc(button) {
+    if (button.id === 'equal') {
+        display.textContent = eval(display.textContent);
+    }
+    else if (button.id === 'clear') {
+        display.textContent = '';
+    }
+    else if (button.id === 'delete') {
+        display.textContent = display.textContent.slice(0, -1);
+    }
+    else {
+        if (display.textContent.length < 20) {
+            display.textContent += button.textContent;
+        }
+    }
+}
+
+button.forEach(button => button.addEventListener('click', () => calc(button)))
